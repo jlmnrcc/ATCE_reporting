7a2ae091ba6ca0fa99b213a3db994f1db60b79c1
Jakob Glarbo Møller <jlm@nordic-rcc.net>
2023-05-25 11:50:54 +0200

    update to use topology.py

--- python/evaluate_cnec_constraints.py
+++ python/evaluate_cnec_constraints.py
@@ -7,7 +7,8 @@ import requests
 import pandas as pd
 import datetime
 import pytz
-import atce_grephical_report
+import topology
+import atce_graphical_report
 
 def get_fb_data(start_time, end_time):
 
@@ -43,79 +44,13 @@ def dropTolerance(B,tol):
 
 
 if __name__=="__main__":
-    bzbs = [{'from':"DK1", 'to':"DK1_CO", 'borderDirection':"DK1-DK1_CO", "ig107name": "DK1-DK1_CO"},
-            {'from':"DK1", 'to':"DK1_DE", 'borderDirection':"DK1-DK1_DE", "ig107name": "DK1-DK1_DE"},
-            {'from':"DK1", 'to':"DK1_KS", 'borderDirection':"DK1-DK1_KS", "ig107name": "DK1A-SE3"},
-            {'from':"DK1", 'to':"DK1_SK", 'borderDirection':"DK1-DK1_SK", "ig107name": "DK1-DK1_SK"},
-            {'from':"DK1", 'to':"DK1_SB", 'borderDirection':"DK1-DK1_SB", "ig107name": "DK1-DK2"},
-            {'from':"DK1_CO", 'to':"DK1", 'borderDirection':"DK1_CO-DK1", "ig107name": "DK1_CO-DK1"},
-            {'from':"DK1_DE", 'to':"DK1", 'borderDirection':"DK1_DE-DK1", "ig107name": "DK1_DE-DK1"},
-            {'from':"DK1_KS", 'to':"DK1", 'borderDirection':"DK1_KS-DK1", "ig107name": "SE3-DK1A"},
-            {'from':"DK1_SK", 'to':"DK1", 'borderDirection':"DK1_SK-DK1", "ig107name": "NO2-DK1A"},
-            {'from':"DK1_SB", 'to':"DK1", 'borderDirection':"DK1_SB-DK1" ,"ig107name": "DK2-DK1"},
-            {'from':"DK2", 'to':"DK2_KO", 'borderDirection':"DK2-DK2_KO" ,"ig107name": "DK2-DK2_KO"},
-            {'from':"DK2", 'to':"DK2_SB", 'borderDirection':"DK2-DK2_SB" ,"ig107name": "DK2-DK1"},
-            {'from':"DK2", 'to':"SE4", 'borderDirection':"DK2-SE4", "ig107name": "DK2-SE4"},
-            {'from':"DK2_KO", 'to':"DK2", 'borderDirection':"DK2_KO-DK2", "ig107name": "DK2_KO-DK2"},
-            {'from':"DK2_SB", 'to':"DK2", 'borderDirection':"DK2_SB-DK2", "ig107name": "DK1-DK2"},
-            {'from':"FI", 'to':"FI_EL", 'borderDirection':"FI-FI_EL", "ig107name": "FI-FI_EL"},
-            {'from':"FI", 'to':"FI_FS", 'borderDirection':"FI-FI_FS", "ig107name": "FI-SE3"},
-            {'from':"FI", 'to':"NO4", 'borderDirection':"FI-NO4", "ig107name": "FI-NO4"},
-            {'from':"FI", 'to':"SE1", 'borderDirection':"FI-SE1", "ig107name": "FI-SE1"},
-            {'from':"FI_EL", 'to':"FI", 'borderDirection':"FI_EL-FI", "ig107name": "FI_EL-FI"},
-            {'from':"FI_FS", 'to':"FI", 'borderDirection':"FI_FS-FI", "ig107name": "SE3-FI"},
-            {'from':"NO1", 'to':"NO2", 'borderDirection':"NO1-NO2", "ig107name": "NO1A-NO2"},
-            {'from':"NO1", 'to':"NO3", 'borderDirection':"NO1-NO3", "ig107name": "NO1-NO3"},
-            {'from':"NO1", 'to':"NO5", 'borderDirection':"NO1-NO5", "ig107name": "NO1A-NO5"},
-            {'from':"NO1", 'to':"SE3", 'borderDirection':"NO1-SE3", "ig107name": "NO1-SE3"},
-            {'from':"NO2", 'to':"NO1", 'borderDirection':"NO2-NO1", "ig107name": "NO2-NO1A"},
-            {'from':"NO2", 'to':"NO2_ND", 'borderDirection':"NO2-NO2_ND", "ig107name": "NO2-NO2_ND"},
-            {'from':"NO2", 'to':"NO2_NK", 'borderDirection':"NO2-NO2_NK", "ig107name": "NO2-NO2_NK"},
-            {'from':"NO2", 'to':"NO2_SK", 'borderDirection':"NO2-NO2_SK", "ig107name": "NO2-NO2_SK"},
-            {'from':"NO2", 'to':"NO5", 'borderDirection':"NO2-NO5", "ig107name": "NO2-NO5"},
-            {'from':"NO2_ND", 'to':"NO2", 'borderDirection':"NO2_ND-NO2", "ig107name": "NO2_ND-NO2"},
-            {'from':"NO2_NK", 'to':"NO2", 'borderDirection':"NO2_NK-NO2", "ig107name": "NO2_NK-NO2"},
-            {'from':"NO2_SK", 'to':"NO2", 'borderDirection':"NO2_SK-NO2", "ig107name": "DK1A-NO2"},
-            {'from':"NO3", 'to':"NO1", 'borderDirection':"NO3-NO1", "ig107name": "NO3-NO1"},
-            {'from':"NO3", 'to':"NO4", 'borderDirection':"NO3-NO4", "ig107name": "NO3-NO4"},
-            {'from':"NO3", 'to':"NO5", 'borderDirection':"NO3-NO5", "ig107name": "NO3-NO5"},
-            {'from':"NO3", 'to':"SE2", 'borderDirection':"NO3-SE2", "ig107name": "NO3-SE2"},
-            {'from':"NO4", 'to':"FI", 'borderDirection':"NO4-FI", "ig107name": "NO4-FI"},
-            {'from':"NO4", 'to':"NO3", 'borderDirection':"NO4-NO3", "ig107name": "NO4-NO3"},
-            {'from':"NO4", 'to':"SE1", 'borderDirection':"NO4-SE1", "ig107name": "NO4-SE1"},
-            {'from':"NO4", 'to':"SE2", 'borderDirection':"NO4-SE2", "ig107name": "NO4-SE2"},
-            {'from':"NO5", 'to':"NO1", 'borderDirection':"NO5-NO1", "ig107name": "NO5-NO1A"},
-            {'from':"NO5", 'to':"NO2", 'borderDirection':"NO5-NO2", "ig107name": "NO5-NO2"},
-            {'from':"NO5", 'to':"NO3", 'borderDirection':"NO5-NO3", "ig107name": "NO5-NO3"},
-            {'from':"SE1", 'to':"FI", 'borderDirection':"SE1-FI", "ig107name": "SE1-FI"},
-            {'from':"SE1", 'to':"NO4", 'borderDirection':"SE1-NO4", "ig107name": "SE1-NO4"},
-            {'from':"SE1", 'to':"SE2", 'borderDirection':"SE1-SE2", "ig107name": "SE1-SE2"},
-            {'from':"SE2", 'to':"NO3", 'borderDirection':"SE2-NO3", "ig107name": "SE2-NO3"},
-            {'from':"SE2", 'to':"NO4", 'borderDirection':"SE2-NO4", "ig107name": "SE2-NO4"},
-            {'from':"SE2", 'to':"SE1", 'borderDirection':"SE2-SE1", "ig107name": "SE2-SE1"},
-            {'from':"SE2", 'to':"SE3", 'borderDirection':"SE2-SE3", "ig107name": "SE2-SE3"},
-            {'from':"SE3", 'to':"NO1", 'borderDirection':"SE3-NO1", "ig107name": "SE3-NO1"},
-            {'from':"SE3", 'to':"SE2", 'borderDirection':"SE3-SE2", "ig107name": "SE3-SE2"},
-            {'from':"SE3", 'to':"SE3_FS", 'borderDirection':"SE3-SE3_FS", "ig107name": "SE3-FI"},
-            {'from':"SE3", 'to':"SE3_KS", 'borderDirection':"SE3-SE3_KS", "ig107name": "SE3-DK1A"},
-            {'from':"SE3", 'to':"SE3_SWL", 'borderDirection':"SE3-SE3_SWL", "ig107name": "SE3-SE4-DC"},
-            {'from':"SE3", 'to':"SE4", 'borderDirection':"SE3-SE4", "ig107name": "SE3-SE4"},
-            {'from':"SE3_FS", 'to':"SE3", 'borderDirection':"SE3_FS-SE3", "ig107name": "FI-SE3"},
-            {'from':"SE3_KS", 'to':"SE3", 'borderDirection':"SE3_KS-SE3", "ig107name": "DK1A-SE3"},
-            {'from':"SE3_SWL", 'to':"SE3", 'borderDirection':"SE3_SWL-SE3", "ig107name": "SE3_SWL-SE3"},
-            {'from':"SE4", 'to':"DK2", 'borderDirection':"SE4-DK2", "ig107name": "SE4-DK2"},
-            {'from':"SE4", 'to':"SE3", 'borderDirection':"SE4-SE3", "ig107name": "SE4-SE3"},
-            {'from':"SE4", 'to':"SE4_BC", 'borderDirection':"SE4-SE4_BC", "ig107name": "SE4-SE4_BC"},
-            {'from':"SE4", 'to':"SE4_NB", 'borderDirection':"SE4-SE4_NB", "ig107name": "SE4-SE4_NB"},
-            {'from':"SE4", 'to':"SE4_SP", 'borderDirection':"SE4-SE4_SP", "ig107name": "SE4-SE4_SP"},
-            {'from':"SE4", 'to':"SE4_SWL", 'borderDirection':"SE4-SE4_SWL", "ig107name": "SE4-SE4_SWL"},
-            {'from':"SE4_BC", 'to':"SE4", 'borderDirection':"SE4_BC-SE4", "ig107name": "SE4_BC-SE4"},
-            {'from':"SE4_NB", 'to':"SE4", 'borderDirection':"SE4_NB-SE4", "ig107name": "SE4_NB-SE4"},
-            {'from':"SE4_SP", 'to':"SE4", 'borderDirection':"SE4_SP-SE4", "ig107name": "SE4_SP-SE4"},
-            {'from':"SE4_SWL", 'to':"SE4", 'borderDirection':"SE4_SWL-SE4", "ig107name": "SE4_SWL-SE4"}]
-
+    
+    path = "..\\data\\ATCEvalidationToolTestData"
+    
+    bzbs = [b for b in topology.latest_topology["borders"] if b['inFbTopology']]
+    
     print("Importing ATCE results...")
-    atcdf = atce_grephical_report.readATCEextracts("..\\data\\smaller_tolerance")
+    atcdf = atce_graphical_report.readATCEextracts(path)
     print("... done!")
     
     start = atcdf["dateTime"].min()
@@ -123,9 +58,9 @@ if __name__=="__main__":
     
     print('Querying FB data from JAO...')
     fbdf = get_fb_data(start, end)
-    fbdf.to_csv('jao_data.csv')
-    # fbdf = pd.read_csv("jao_data.csv")
-    # fbdf["dateTimeUtc"] = pd.to_datetime(fbdf["dateTimeUtc"], format="%Y-%m-%d %H:%M:00+00:00", utc=True)
+    fbdf.to_csv(path + '\\' + 'jao_data.csv')
+    # fbdf = pd.read_csv( path + '\\' + "jao_data.csv")
+    fbdf["dateTimeUtc"] = pd.to_datetime(fbdf["dateTimeUtc"], format="%Y-%m-%d %H:%M:00+00:00", utc=True)
     
     fbdf = fbdf.sort_values(by='dateTimeUtc',ascending=True)
     
@@ -135,14 +70,20 @@ if __name__=="__main__":
     fb_bzb_order = []
     
     for brb in bzbs:
-        fb_bzb_order.append('z2z_ptdf_' + brb['borderDirection'])
-        atce_bzb_order.append(brb['ig107name'])
-        fbdf["z2z_ptdf_"+brb['borderDirection']]=fbdf["ptdf_"+brb['from']]-fbdf["ptdf_"+brb['to']]
-        if not brb['ig107name'] in atcdf:
-            print("Warning! Bidding zone: " + brb['borderDirection'] + " (" + brb['ig107name'] + ") was not found in ATCE result data. Missing values are treated as 0")
-            atcdf[brb['ig107name'], 'AAC'] = [0] * len(atcdf["dateTime"])
-            atcdf[brb['ig107name'], 'NTC_final'] = [0] * len(atcdf["dateTime"])
-    
+        fb_bzb_order.append('z2z_ptdf_' + brb['name'])
+        fbdf["z2z_ptdf_"+brb['name']]=fbdf["ptdf_"+brb['from']]-fbdf["ptdf_"+brb['to']]
+        
+        if len(brb['mappedNTCborder'])>0:
+            atce_name = brb['mappedNTCborder'][0]
+        else:
+            atce_name = brb['name']
+        atce_bzb_order.append(atce_name) 
+        if not atce_name in atcdf:
+            print("Warning! Bidding zone: " + atce_name + " was not found in ATCE result data. Missing values are treated as 0")
+            atcdf[atce_name, 'AAC'] = [0] * len(atcdf["dateTime"])
+            atcdf[atce_name, 'NTC_final'] = [0] * len(atcdf["dateTime"])
+            
+          
     dropTol = 0.05
     
     f_aac0 = np.array([0.0]*len(fbdf['id']))
@@ -157,7 +98,7 @@ if __name__=="__main__":
     A_zer = dropTolerance(A, 1e-16)
     A_tol = A_tol.transpose()
     A_zer = A_zer.transpose()
-
+    
     for mtu in atcdf['dateTime']:
         print('... ' + mtu.strftime('%Y-%m-%d %H:%M') + ' ...')
         cnec_idx = [ii for ii in fbdf.index if fbdf["dateTimeUtc"][ii]==mtu]#todo
@@ -168,6 +109,7 @@ if __name__=="__main__":
         f_aac_tol[cnec_idx] = aacs * A_tol[:,cnec_idx]
         AAC_ntc_0[cnec_idx] = ntcs * A_zer[:,cnec_idx]
         AAC_ntc_tol[cnec_idx] = ntcs * A_tol[:,cnec_idx]
+        
     
     fbdf['f_aac0'] = f_aac0
     fbdf['f_aac_tol'] = f_aac_tol
@@ -181,7 +123,7 @@ if __name__=="__main__":
  
     print('Writing output...')
 
-    fbdf.to_csv("out.csv")
+    fbdf.to_csv(path + "\\atce_validation_prototype.csv")
     print('done!')
     
     
